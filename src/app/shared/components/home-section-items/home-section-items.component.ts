0abd8baaaeb632dda90dfb4051bb3b49033b2c27
ritikparihar <ritikparihar111@gmail.com>
2021-12-11 19:21:46 +0530

    some ui improvements/added why choose us section

---
+++ src/app/shared/components/home-section-items/home-section-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-section-items',
+  templateUrl: './home-section-items.component.html',
+  styleUrls: ['./home-section-items.component.scss']
+})
+export class HomeSectionItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
